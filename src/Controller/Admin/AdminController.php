c89984ca4d432573e9bc935ed14374e85fcd36e9
Jonathan Levy <jonathan.levy2@etu.univ-paris1.fr>
2025-02-06 10:52:05 +0100

    ajout pages admin et stock - favicon - et modifs

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -6,44 +6,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminDashboardController extends AbstractController
+class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'admin_dashboard')]
+    #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig');
     }
 
-    #[Route('/admin/stock', name: 'admin_stock')]
+    #[Route('/admin/stock', name: 'app_admin_stock')]
     public function stock(): Response
     {
         $produits = [
             [
                 'nom' => 'Shampoing',
-                'marque' => 'Kérastase',
-                'description' => 'Après-shampoing L\'Oréal',
-                'quantite' => 9,
+                'marque' => 'L\'Oréal',
+                'description' => 'Shampoing nourrissant',
+                'quantite' => 10,
                 'prix_unitaire' => 30,
             ],
             [
-                'nom' => 'Shampoing',
+                'nom' => 'Après-shampoing',
                 'marque' => 'Madara',
-                'description' => 'Kérastase',
-                'quantite' => 3,
+                'description' => 'Après-shampoing hydratant',
+                'quantite' => 8,
                 'prix_unitaire' => 45,
             ],
             [
                 'nom' => 'Shampoing',
                 'marque' => 'Kérastase',
-                'description' => '',
+                'description' => 'Shampoing réparateur',
                 'quantite' => 5,
                 'prix_unitaire' => 29,
             ],
             [
-                'nom' => '',
-                'marque' => '',
-                'description' => '',
-                'quantite' => 2,
+                'nom' => 'Huile capillaire',
+                'marque' => 'L\'Oréal',
+                'description' => 'Huile nourrissante pour cheveux',
+                'quantite' => 3,
                 'prix_unitaire' => 25,
             ],
         ];
