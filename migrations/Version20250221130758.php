563b6888ba9a2a822b7cd9f0aa3d420e8e8c8243
Xavier Alibert <xavier.alibert@etu-paris1.fr>
2025-02-21 14:18:47 +0100

    supp version

--- migrations/Version20250221130758.php
+++ migrations/Version20250221130758.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250220154608 extends AbstractMigration
+final class Version20250221130758 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20250220154608 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE review CHANGE note rating INT NOT NULL');
         $this->addSql('ALTER TABLE user ADD first_name VARCHAR(255) NOT NULL, ADD last_name VARCHAR(255) NOT NULL, DROP name, DROP surname');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `user` ADD name VARCHAR(255) NOT NULL, ADD surname VARCHAR(255) NOT NULL, DROP first_name, DROP last_name');
+        $this->addSql('ALTER TABLE user ADD name VARCHAR(255) NOT NULL, ADD surname VARCHAR(255) NOT NULL, DROP first_name, DROP last_name');
+        $this->addSql('ALTER TABLE review CHANGE rating note INT NOT NULL');
     }
 }
